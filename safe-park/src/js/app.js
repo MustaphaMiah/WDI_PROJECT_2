346a1e217b5fc6bf632f9231c3bd16bf262f0e38
MustaphaMiah <muhammad_mustapha_miah@live.co.uk>
2017-01-16 13:36:33 +0000

    custom markers and animation

--- safe-park/src/js/app.js
+++ safe-park/src/js/app.js
@@ -49,9 +49,9 @@ App.addMarkerForCrime = function(crime) {
   const latlng = new google.maps.LatLng(parseFloat(crime.location.latitude), parseFloat(crime.location.longitude));
   const marker = new google.maps.Marker({
     position: latlng,
-    map: App.map
-    // icon: '../images/marker.png',
-    // animation: google.maps.Animation.DROP
+    map: App.map,
+    icon: '../images/marker.png',
+    animation: google.maps.Animation.DROP
   });
 
   App.addInfoWindowForCrime(crime, marker);
@@ -86,93 +86,93 @@ App.loggedInState = function() {
   this.createMap.bind(this)();
 };
 
-  App.loggedOutState = function(){
-    $('.loggedIn').hide();
-    $('.loggedOut').show();
-    this.register();
-  };
+App.loggedOutState = function(){
+  $('.loggedIn').hide();
+  $('.loggedOut').show();
+  this.register();
+};
 
-  App.register = function(e){
-    if (e) e.preventDefault();
-    this.$main.html(`
-      <h2>Register</h2>
-      <form method="post" action="/register">
-      <div class="form-group">
-      <input class="form-control" type="text" name="user[username]" placeholder="Username">
-      </div>
-      <div class="form-group">
-      <input class="form-control" type="email" name="user[email]" placeholder="Email">
-      </div>
-      <div class="form-group">
-      <input class="form-control" type="password" name="user[password]" placeholder="Password">
-      </div>
-      <div class="form-group">
-      <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
-      </div>
-      <input class="btn btn-primary" type="submit" value="Register">
-      </form>
-      `);
-    };
+App.register = function(e){
+  if (e) e.preventDefault();
+  this.$main.html(`
+    <h2>Register</h2>
+    <form method="post" action="/register">
+    <div class="form-group">
+    <input class="form-control" type="text" name="user[username]" placeholder="Username">
+    </div>
+    <div class="form-group">
+    <input class="form-control" type="email" name="user[email]" placeholder="Email">
+    </div>
+    <div class="form-group">
+    <input class="form-control" type="password" name="user[password]" placeholder="Password">
+    </div>
+    <div class="form-group">
+    <input class="form-control" type="password" name="user[passwordConfirmation]" placeholder="Password Confirmation">
+    </div>
+    <input class="btn btn-primary" type="submit" value="Register">
+    </form>
+    `);
+};
 
-    App.login = function(e) {
-      e.preventDefault();
-      this.$main.html(`
-        <h2>Login</h2>
-        <form method="post" action="/login">
-        <div class="form-group">
-        <input class="form-control" type="email" name="email" placeholder="Email">
-        </div>
-        <div class="form-group">
-        <input class="form-control" type="password" name="password" placeholder="Password">
-        </div>
-        <input class="btn btn-primary" type="submit" value="Login">
-        </form>
-        `);
-      };
+App.login = function(e) {
+  e.preventDefault();
+  this.$main.html(`
+    <h2>Login</h2>
+    <form method="post" action="/login">
+    <div class="form-group">
+    <input class="form-control" type="email" name="email" placeholder="Email">
+    </div>
+    <div class="form-group">
+    <input class="form-control" type="password" name="password" placeholder="Password">
+    </div>
+    <input class="btn btn-primary" type="submit" value="Login">
+    </form>
+    `);
+};
 
-      App.logout = function(e){
-        e.preventDefault();
-        this.removeToken();
-        this.loggedOutState();
-      };
+App.logout = function(e){
+  e.preventDefault();
+  this.removeToken();
+  this.loggedOutState();
+};
 
-      App.homepage = function(){
-      };
+App.homepage = function(){
+};
 
-      App.handleForm = function(e){
-        e.preventDefault();
-        const url    = `${App.apiUrl}${$(this).attr('action')}`;
-        const method = $(this).attr('method');
-        const data   = $(this).serialize();
-        return App.ajaxRequest(url, method, data, data => {
-          if (data.token) App.setToken(data.token);
-          App.loggedInState();
-        });
-      };
+App.handleForm = function(e){
+  e.preventDefault();
+  const url    = `${App.apiUrl}${$(this).attr('action')}`;
+  const method = $(this).attr('method');
+  const data   = $(this).serialize();
+  return App.ajaxRequest(url, method, data, data => {
+    if (data.token) App.setToken(data.token);
+    App.loggedInState();
+  });
+};
 
-      App.ajaxRequest = function(url, method, data, callback){
-        return $.ajax({
-          url,
-          method,
-          data,
-          beforeSend: this.setRequestHeader.bind(this)
-        })
-        .done(callback)
-        .fail(data => {
-          console.log(data);
-        });
-      };
+App.ajaxRequest = function(url, method, data, callback){
+  return $.ajax({
+    url,
+    method,
+    data,
+    beforeSend: this.setRequestHeader.bind(this)
+  })
+  .done(callback)
+  .fail(data => {
+    console.log(data);
+  });
+};
 
-      App.setRequestHeader = function(xhr) {
-        return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
-      };
-      App.setToken = function(token){
-        return window.localStorage.setItem('token', token);
-      };
-      App.getToken = function(){
-        return window.localStorage.getItem('token');
-      };
-      App.removeToken = function(){
-        return window.localStorage.clear();
-      };
-      $(App.init.bind(App));
+App.setRequestHeader = function(xhr) {
+  return xhr.setRequestHeader('Authorization', `Bearer ${this.getToken()}`);
+};
+App.setToken = function(token){
+  return window.localStorage.setItem('token', token);
+};
+App.getToken = function(){
+  return window.localStorage.getItem('token');
+};
+App.removeToken = function(){
+  return window.localStorage.clear();
+};
+$(App.init.bind(App));
